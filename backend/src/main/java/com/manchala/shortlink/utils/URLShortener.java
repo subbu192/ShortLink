d51df817c885bd3d048a6b71ce37f590aff0b4c4
subbu192 <manchalasubramanyam2@gmail.com>
2024-12-22 10:27:03 +0530

    Added Core URLShortener BoilerPlate

---
+++ backend/src/main/java/com/manchala/shortlink/utils/URLShortener.java
@@ -0,0 +1,11 @@
+package com.manchala.shortlink.utils;
+
+public class URLShortener {
+
+    public static String convertIdToShortUrl(Long urlId) {
+        return null;
+    }
+    public static Long convertShortUrlToId(String shortUrl) {
+        return null;
+    }
+}
